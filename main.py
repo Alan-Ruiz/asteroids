bd56f2b4166adcae5e7fbc315fa1879ea2f52219
Alan Rodriguez <gheryk90@gmail.com>
2024-06-13 20:07:27 +0200

    ship moves to left slowly =D!

--- main.py
+++ main.py
@@ -25,32 +25,49 @@ bg = pygame.image.load(os.path.join('images', 'bg.jpg'))
 debris = pygame.image.load(os.path.join('images', 'debris2_brown.png'))
 ship = pygame.image.load(os.path.join('images', 'ship.png'))
 
+ship_x = WIDTH/2 - 50
+ship_y = HEIGHT/2 - 50
+ship_angle = 0
+
+def rot_center(image, angle):
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
+
 #draw game function
 def draw(canvas):
-  global time
-  canvas.fill(BLACK)
-  canvas.blit(bg,(0,0))
-  canvas.blit(debris,(time*.3,0))
-  canvas.blit(debris,(time*.3-WIDTH, 0))
-  time = time +1
-  canvas.blit(ship, (WIDTH/2 -50, HEIGHT/2 -50))
+    global time
+    canvas.fill(BLACK)
+    canvas.blit(bg,(0,0))
+    canvas.blit(debris,(time*.3,0))
+    canvas.blit(debris,(time*.3-WIDTH, 0))
+    time = time +1
+    canvas.blit(rot_center(ship, ship_angle), (ship_x, ship_y))
 
 #handle input function
 def handle_input():
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+    global ship_angle
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                ship_angle = ship_angle + 1
       
 def update_screen():
-  pygame.display.update()
-  fps.tick(60)
+    pygame.display.update()
+    fps.tick(60)
 
 
 # asteroids game loop 
 while True:
-  draw(window)
-  handle_input()
+    draw(window)
+    handle_input()
   
-  #game logic
-  update_screen()
+    #game logic
+    update_screen()
